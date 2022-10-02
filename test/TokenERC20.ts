d4ceaad918a167d90872ffc0cc5d657bf8862b9e
elcomendante <karol@finsane.com>
2022-10-02 23:18:26 +0100

    deploy tests done

--- test/TokenERC20.ts
+++ test/TokenERC20.ts
@@ -6,30 +6,35 @@ import { ethers } from "hardhat";
 
 describe("TokenERC20", function () {
   async function deployTokenERC20() {
-    const totalSupply = "10000000000";
     const [owner, acc1, acc2] = await ethers.getSigners();
     const TokenERC20 = await ethers.getContractFactory("TokenERC20");
     const tkn = await TokenERC20.deploy("KinkyToken", "KNK");
 
-    return { totalSupply, owner, acc1, acc2, tkn };
+    return { owner, acc1, acc2, tkn };
   }
 
   describe("Deployment", function () {
     it("Should set the right token name", async function () {
       const { tkn } = await loadFixture(deployTokenERC20);
-
       expect(await tkn.name()).to.equal("KinkyToken");
     });
 
     it("Should set the righ token symbol", async function () {
       const { tkn } = await loadFixture(deployTokenERC20);
-
       expect(await tkn.symbol()).to.equal("KNK");
     });
     it("Should set the right owner", async function () {
       const { tkn, owner } = await loadFixture(deployTokenERC20);
-
       expect(await tkn.contractowner()).to.equal(owner.address);
+    });
+    it("Should set the right supply", async function () {
+      const { tkn } = await loadFixture(deployTokenERC20);
+      expect(await tkn.totalSupply()).to.equal(0);
+    });
+
+    it("Should set the right decimals", async function () {
+      const { tkn } = await loadFixture(deployTokenERC20);
+      expect(await tkn.decimals()).to.equal(18);
     });
   });
 });

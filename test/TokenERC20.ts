22e35e1e9ca8068557da4acb64217639025572dc
elcomendante <karol@finsane.com>
2022-10-02 23:20:27 +0100

    cleaned deps

--- test/TokenERC20.ts
+++ test/TokenERC20.ts
@@ -1,6 +1,4 @@
-import { TokenERC20 } from "../typechain-types";
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 

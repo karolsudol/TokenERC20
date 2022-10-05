d340d7a705b960aacbce1683a29f7137dab1903b
elcomendante <karol@finsane.com>
2022-10-05 12:38:15 +0100

    env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,17 +4,13 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // Add these lines -----------
-    goerli: {
-      url: process.env.GOERLI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+
     // ---------------------------
   },
   gasReporter: {
